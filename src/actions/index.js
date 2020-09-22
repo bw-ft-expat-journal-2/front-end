9cfe041e0e3aefe48e047e9b493d36acdaf67c4f
Maycie Morris <maycie.morris@gmail.com>
2020-09-22 13:45:32 -0400

    Hopefully completed reducer page, no errors as of yet

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,84 @@
 import { axiosWithAuth } from '../utils/axiosWithAuth';
+// import { POSTS_PATH } from '../utils/URLS'
 
 export const FETCH_DATA = 'FETCH_DATA';
 export const FETCH_DATA_SUCCESS = 'POST_DATA_SUCCESS'
 export const POST_STORY = 'POST_STORY';
+export const POST_STORY_SUCCESS = 'POST_STORY_SUCCESS'
 export const EDIT_STORY = 'EDIT_STORY';
+export const EDIT_STORY_SUCCESS = 'EDIT_STORY_SUCCESS'
 export const DELETE_STORY = 'DELETE_STORY';
+export const DELETE_STORY_SUCCESS = 'DELETE_STORY_SUCCESS'
 
-export const fetchData = () => {
-    return dispatch => {
-        dispatch({
-            type: FETCH_DATA
+export const fetchData = ()  => {
+    return (dispatch) => {
+    dispatch({
+        type: FETCH_DATA
+    })
+    axiosWithAuth()
+        .get(`/api/posts`)
+        .then(res => {
+            dispatch({
+                type: FETCH_DATA_SUCCESS,
+                payload: res.data
+            })
         })
-        axiosWithAuth()
-            .get('/api/users')
-            .then(res => {
-                console.log(res.data)
+        .catch(err => {
+            console.log('Yikes', err)
+        })
+    }
+}
+
+export const addStory = post => {
+    const addNewStory = axiosWithAuth()
+        .post(`/api/posts`, post)
+    return (dispatch) => {
+        dispatch({
+            type: POST_STORY
+        })
+        addNewStory
+            .then(({data}) => {
                 dispatch({
-                    type: FETCH_DATA_SUCCESS
+                    type: POST_STORY_SUCCESS,
+                    payload: data
                 })
+            .catch(err => {
+                console.log('No new story :c', err)
+            })
             })
     }
+}
+
+export const editStory = post => dispatch => {
+    dispatch({
+        type: EDIT_STORY
+    })
+    axiosWithAuth()
+        .put(`/api/posts/${post.id}`, post)
+        .then(res => {
+            dispatch({
+                type: EDIT_STORY_SUCCESS, 
+                payload: res.data
+            })
+        })
+}
+
+export const deleteStory = post => {
+    const removePost = axiosWithAuth()
+        .delete(`/api/posts/${post.id}`, post)
+        return (dispatch) => {
+            dispatch({
+                type: DELETE_STORY
+            })
+        removePost
+        .then(({data}) => {
+            dispatch({
+                type: DELETE_STORY_SUCCESS,
+                payload: data
+            })
+        })
+        .catch(err => {
+            console.log(`That didn't delete :/`, err)
+        })
+     }
 }
