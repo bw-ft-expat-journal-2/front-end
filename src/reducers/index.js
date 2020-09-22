fb7042fa7e0f4193231ecf1396b51115b0205104
Maycie Morris <maycie.morris@gmail.com>
2020-09-22 09:40:54 -0400

    Added framework for edit/deleting into reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,43 @@
+// import { bindActionCreators } from "redux"
+import { FETCH_DATA, POST_STORY, FETCH_DATA_SUCCESS, EDIT_STORY, DELETE_STORY } from '../actions'
+
 const initialState = {
-	data: []
+	data: [],
+	loading: false,
+	error: ""
 }
 
-export default (state = initialState, { type, payload }) => {
-	switch (type) {
-		case 'FETCH_DATA':
+export default (state = initialState, action) => {
+	switch (action.type) {
+		case FETCH_DATA:
 			console.log('REDUCER FETCH_DATA', payload)
 			return {
 				...state,
-				data: payload
+				data: payload,
+				loading: true
 			}
-		case 'POST_STORY':
+			case FETCH_DATA_SUCCESS:
+				return {
+					...state,
+					data: action.payload,
+					loading: false,
+					error: ""
+				}
+		case POST_STORY:
 			return {
 				...state,
-				data: [ ...state.data, payload ]
+				data: [ ...state.data, payload ],
+				loading: true
+			}
+		case EDIT_STORY:
+			return {
+				...state,
+				data: action.payload
+			}
+		case DELETE_STORY:
+			return {
+				...state,
+				data: action.payload
 			}
 
 		default:
