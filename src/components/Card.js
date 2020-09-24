69d353693922a69a9ef4ea73609575bdfbc05d3b
teranasoras <tmayn1234@gmail.com>
2020-09-24 15:56:18 -0500

    styled the card page to look a bit better

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,7 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 const CardStyle = styled.div`
     border:2px solid grey;
     width: 75%;
-    height:30re;
+    height:30rem;
     box-sizing: border-box;
     margin-bottom:2rem;
     background-color: #1f2833;
